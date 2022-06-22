51e2e3dc5cae6bdd7104da795b3abfa7d89959f6
anıl altan <anlaltnn@gmail.com>
2022-06-22 15:28:27 +0300

    first commit

--- components/PostList.js
+++ components/PostList.js
@@ -8,7 +8,7 @@ const PostList = ({ posts }) => {
         <>
             <Grid>
                 {posts.data.map(post => (
-                    <Post post={post}/>))}
+                    <Post post={post} key={post.id}/>))}
             </Grid>
             
         </>
